7d794d5438388abb909499bdcf689c3f4f386c9f
wkq <172137365@qq.com>
2019-07-04 19:42:26 +0800

    DemoMultiProject为启动项目，DllProject生成被DemoMultiProject调用的dll

--- DemoMultiProject/Program.cs
+++ DemoMultiProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DllProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace DemoMultiProject
     {
         static void Main(string[] args)
         {
+            Class1 c = new Class1();
         }
     }
 }
---
+++ DllProject/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllProject
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        public static void Print()
+        {
+            Console.WriteLine(123);
+            Console.ReadLine();
+        }
+    }
+}
